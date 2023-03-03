385f00fc35ab29c63f086d6eb8485b6301006982
Derryl Taufik <derryldeveloper@gmail.com>
2023-03-04 00:28:24 +0700

    updated environment variables

--- lib/data/data_sources/remote_storage_service.dart
+++ lib/data/data_sources/remote_storage_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:tocopedia/common/env_variables.dart';
 
 abstract class RemoteStorageService {
   Future<String> uploadImage(File imageFile, {String? folderName});
@@ -9,12 +10,9 @@ abstract class RemoteStorageService {
 class RemoteStorageServiceImpl implements RemoteStorageService {
   @override
   Future<String> uploadImage(File imageFile, {String? folderName}) async {
-    const cloudinaryCloudName = String.fromEnvironment('CLOUDINARY_CLOUD_NAME');
-    const cloudinaryUploadPreset =
-        String.fromEnvironment('CLOUDINARY_UPLOAD_PRESET');
     try {
       final cloudinary =
-          CloudinaryPublic(cloudinaryCloudName, cloudinaryUploadPreset);
+          CloudinaryPublic(CLOUDINARY_CLOUD_NAME, CLOUDINARY_UPLOAD_PRESET);
 
       CloudinaryResponse response = await cloudinary.uploadFile(
         CloudinaryFile.fromFile(imageFile.path, folder: folderName),
