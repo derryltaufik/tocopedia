a07d618a605f46500b5b6a07351682dee0a8bb4a
Derryl Taufik <derryldeveloper@gmail.com>
2023-03-18 15:44:05 +0700

    added cors support

--- tocopedia_backend/src/app.js
+++ tocopedia_backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 require("./db/mongoose");
 
@@ -17,6 +18,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(authRouter);
 app.use(userRouter);
